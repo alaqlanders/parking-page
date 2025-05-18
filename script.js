1ca37e4451f5523753de91831724e8bd82ffeb6e
alaqlanders <alaqlanders@gmail.com>
2025-05-17 23:07:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-console.log " ALAQ :) " 
-
+console.warn(`
+    _   _      _   ___    _      _   _  _ ___  ___ ___  ___ 
+   /_\\ | |    /_\\ / _ \\  | |    /_\\ | \\| |   \\| __| _ \\/ __|
+  / _ \\| |__ / _ \\ (_) | | |__ / _ \\| .\` | |) | _||   /\\__ \\
+ /_/ \\_\\____/_/ \\_\\__\\_\\ |____/_/ \\_\\_|\\_|___/|___|_|_\\|___/
                                                             
+This service is provided free, courtesy of ALAQ Landers. 
+Copyright 2025 by ALAQLanders. Unauthorized use of this material is not permitted.
+`);
